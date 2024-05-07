9411f0daffefe01e3cef10c55ea645e4ce58b0b5
Jakarea <jakareaparvez@gmail.com>
2024-05-07 23:00:37 +0600

    build files

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.19.2/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jakareaparvez/Desktop/mobile-app/evChargerv1"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/jakareaparvez/Desktop/mobile-app/evChargerv1/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9kNzk0NTgwMDc3MTI1NjQ3Y2ZjYjBmYTY3MmQ2MmFjODdiYmYyN2Q1Lw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/jakareaparvez/Desktop/mobile-app/evChargerv1/.dart_tool/package_config.json"
