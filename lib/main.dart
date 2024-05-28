33487bfd7e772e3ccfb7e8aa80a5457573cec0c2
Jakarea Parvez <jakareaparvez@gmail.com>
2024-05-28 11:00:49 +0600

    sentry test

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:ev_charger/views/pages/main_frame_screen.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'dart:io';
 
+import 'package:sentry_flutter/sentry_flutter.dart';
+
 Future<void> main() async {
   // Ensures that Flutter widgets and bindings are initialized before the app runs.
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,7 +31,31 @@ Future<void> main() async {
     }
   }
 
-  runApp(const MyApp());
+  await SentryFlutter.init(
+        (options) {
+      options.dsn = 'https://99cf6c242f90c1503f2bf55d41231823@o4507332205019136.ingest.us.sentry.io/4507332207116288';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.profilesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(MyApp()),
+  );
+
+  try {
+    int? test;
+    test! + 3;
+  } catch (exception, stackTrace) {
+    debugPrint("Catch Error");
+    await Sentry.captureException(
+      exception,
+      stackTrace: stackTrace,
+    );
+  }
+
+  // runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
